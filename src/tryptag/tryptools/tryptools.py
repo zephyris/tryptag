3aae327a87fc6694e2356664d685312b5fcc71f0
Ulrich Dobramysl <1979498+ulido@users.noreply.github.com>
2023-05-18 09:53:09 +0100

    Swap anterior / posterior in `plot_morphology_analysis`

--- src/tryptag/tryptools/tryptools.py
+++ src/tryptag/tryptools/tryptools.py
@@ -278,8 +278,8 @@ def plot_morphology_analysis(cell_image, result):
     plt.plot([a[1] for a in result["midline"]], [a[0] for a in result["midline"]], color=(0.8, 0.8, 0.8, 0.2))
   # anterior and posterior points (magenta, yellow)
   if "anterior" in result:
-    plt.scatter([result["anterior"][1]], [result["anterior"][0]], s=10, marker="o", color=(1.0, 1.0, 0.0, 0.5))
-    plt.scatter([result["posterior"][1]], [result["posterior"][0]], s=10, marker="o", color=(1.0, 0.0, 1.0, 0.5))
+    plt.scatter([result["anterior"][1]], [result["anterior"][0]], s=10, marker="o", color=(1.0, 0.0, 1.0, 0.5))
+    plt.scatter([result["posterior"][1]], [result["posterior"][0]], s=10, marker="o", color=(1.0, 1.0, 0.0, 0.5))
   # nearest point on midline to centroid of kinetoplasts and nuclei (dark blue, green)
   if "midline" in result:
     if "objects_k" in result:
