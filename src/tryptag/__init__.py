b013e41679d5f7ae5881902584915a8f5dfe4190
Ulrich Dobramysl <1979498+ulido@users.noreply.github.com>
2025-05-08 10:35:05 +0000

    Update examples

--- src/tryptag/__init__.py
+++ src/tryptag/__init__.py
@@ -1,6 +1,22 @@
 import logging
 import sys
 from .tryptag import TrypTag, FieldImage, CellImage, CellLine
-__all__ = ["TrypTag", "FieldImage", "CellImage", "CellLine"]
+from .datasource import (
+    GeneNotFoundError,
+    InvalidTerminusError,
+    FieldNotFoundError,
+    CellNotFoundError,
+)
+
+__all__ = [
+    "TrypTag",
+    "FieldImage",
+    "CellImage",
+    "CellLine",
+    "GeneNotFoundError",
+    "InvalidTerminusError",
+    "FieldNotFoundError",
+    "CellNotFoundError",
+]
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
